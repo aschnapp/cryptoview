731559c658386e28b1708404d8bf02c5a450fc0d
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-09-23 16:01:31 -0700

    Basic setup

--- server/app.js
+++ server/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const request = require('request');
+const path = require('path');
 const app = express();
+const serveStatic = require('serve-static');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(serveStatic(path.resolve(__dirname, '../build/')))
+}
 // TODO: Fix GraphQL Endpoint. Schema is not structured correctly
 // const schema = require('../graphql/schema/schema.js');
 
@@ -11,26 +16,13 @@ const app = express();
 //   graphiql: true
 // }));
 
-app.use(function (req, res, next) {
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-      next();
-  });
-
-
 app.get('/api/charts/marketprice', (req,res) => {
   //Testing out endpoints for now. TODO: Clean up 
   request(' https://api.blockchain.info/charts/market-price', (error, response, body) => {
-    if (error) {
-      res.status(405).send(error);
-    } else {
-      res.status(200).send(body);
-    }
+    if (error) return res.status(405).send(error);
+    res.status(200).send(body);
   });
-
 });
-
 
 app.listen(4000);
 console.log('Listening on 4000...')
